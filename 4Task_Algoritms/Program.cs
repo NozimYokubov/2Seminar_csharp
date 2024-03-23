8b1ab850d9f3991c1552f5509f438d3a1a0643af
Nozim Yokubov <anorcomander@gmail.com>
2024-03-23 17:09:25 +0500

    Решение 4 задачи по семинарам алгоритмы

---
+++ 4Task_Algoritms/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью слева цифру заданного числа или сообщает, что третьей цифры нет.
+// 897 => 7
+// 91 => цифры нет
+// 7918 => 1
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    num = num * -1;
+}
+
+if (num > 99)
+{
+    while (num > 999)
+    {
+        num = num /10;
+    }
+    Console.WriteLine($"Третья цифра = {num % 10}");
+}
+else
+{
+    Console.WriteLine("цифры нет");
+}
