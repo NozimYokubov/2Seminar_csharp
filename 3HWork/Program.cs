a8c8ef018d9b0b8b90614357f9c8d5f3db773be2
Nozim Yokubov <anorcomander@gmail.com>
2024-03-23 17:50:26 +0500

    Выполнил домашнее задание 3 по семинару по простым алгоритмам

---
+++ 3HWork/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (10 <= num && num <= 99)
+{
+    int digit1 = num % 10;
+    int digit2 = num / 10 % 10;
+    if (digit1 > digit2)
+    {
+        Console.WriteLine(digit1);
+    }
+    else
+    {
+        Console.WriteLine(digit2);
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка!");
+}
