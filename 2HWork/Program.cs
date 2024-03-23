7a8beaa798009195a2861a65c29c5a59e33c25df
Nozim Yokubov <anorcomander@gmail.com>
2024-03-23 17:34:03 +0500

    2 Домашнее задание по семинару простых алгаритмов

---
+++ 2HWork/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if ( num1 > 0 && num2 > 0) Console.WriteLine(1);
+else if ( num1 > 0 && num2 < 0) Console.WriteLine(2);
+else if ( num1 < 0 && num2 > 0) Console.WriteLine(3);
+else if ( num1 < 0 && num2 < 0) Console.WriteLine(4);
+else Console.WriteLine("Ошибка!");
