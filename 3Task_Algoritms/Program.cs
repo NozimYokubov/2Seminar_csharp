0869ca77ddf4f6a98c50d2bba41a8931893679da
Nozim Yokubov <anorcomander@gmail.com>
2024-03-23 15:43:28 +0500

    добавили 3 задание семинара алгаритмов

---
+++ 3Task_Algoritms/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, 
+// которая будет принимать на вход два числа и выводить, 
+// кратно ли первое число второму. Если первое число некратно второму, 
+// то программа выводит остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+int num1, num2; //объявление переменных
+
+Console.WriteLine("Введите первое число");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+num2 = Convert.ToInt32(Console.ReadLine());
+int result = num1 % num2;
+
+if (result > 0)
+{
+    Console.WriteLine("нет, " + result);
+}
+else
+{
+    Console.WriteLine("да");
+}
