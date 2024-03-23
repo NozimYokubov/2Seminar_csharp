20a3e92edcd67e35ec8bab1682e792351b7de841
Nozim Yokubov <anorcomander@gmail.com>
2024-03-23 20:37:21 +0500

    4 домашнее задание выполненно по семинару по простым алгоритмам, но цифры выдает в обратном порядке так как не разобрался пока как делать правильно

---
+++ 4HWork/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+//Пример 
+//568  => 5,6,8 
+//8  => 8 
+//9542  => 9,5,4,2
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+if (num > 0)
+{
+    while (num > 0)
+    {
+        i = num % 10;
+        num = num / 10;
+        Console.Write(i + ",");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка!");
+}
+
